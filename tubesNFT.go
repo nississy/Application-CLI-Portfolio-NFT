ac9d569b5049f8966858105faaf7ced7247c339e
nississy <theunknownbusiness1010@gmail.com>
2025-05-12 19:57:21 +0700

    Tambah untuk pilih opsi

--- tubesNFT.go
+++ tubesNFT.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var opsi int
 	menuAwal()
+	opsi = pilihOpsi(opsi)
 }
 
 func menuAwal() {
@@ -12,6 +14,14 @@ func menuAwal() {
 	fmt.Println("====================================")
 	fmt.Println("   1. Tambah Aset CFT")
 	fmt.Println("   2. Lihat Aset")
-	fmt.Println("   3. Keluar")
+	fmt.Println("   3. Lihat Total Aset")
+	fmt.Println("   4. Hapus aset")
+	fmt.Println("   5. Keluar")
 	fmt.Println("------------------------------------")
+}
+
+func pilihOpsi(opsi int) int {
+	fmt.Print("   Silahkan pilih opsi: ")
+	fmt.Scan(&opsi)
+	return opsi
 }
