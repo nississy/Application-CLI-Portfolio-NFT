9cb500609629b2c59a74a18a48064cfab1f661ca
nississy <theunknownbusiness1010@gmail.com>
2025-05-12 19:23:05 +0700

    Tambah Menu

--- tubesNFT.go
+++ tubesNFT.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	var test int
-	var test3 int
+	menuAwal()
+}
+
+func menuAwal() {
+	fmt.Println("====================================")
+	fmt.Println("   [ Aplikasi CLI Portfolio NFT ]   ")
+	fmt.Println("====================================")
+	fmt.Println("   1. Tambah Aset CFT")
+	fmt.Println("   2. Lihat Aset")
+	fmt.Println("   3. Keluar")
+	fmt.Println("------------------------------------")
 }
